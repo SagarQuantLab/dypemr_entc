dd2d58a01ef51026a7058814e7df51843358b020
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-02-17 11:50:18 +0530

    add string functions

---
+++ pythonBasics/session1_python_basics.py
@@ -0,0 +1,19 @@
+# string and its operation
+my_string = "Hello this is College"
+print(my_string)
+
+# print string reverse
+reverse_string = my_string[::-1]
+print(reverse_string)
+
+# access first element
+print(my_string[0])
+print(my_string[1])
+print(my_string[0:3])
+print(my_string[6:10])
+print(my_string[:-4:-1])
+print(my_string.count('H'))
+print(my_string*10)
+print(my_string.replace("H", "Z"))
+print(my_string.lower())
+print(my_string.upper())
